b8510241224428b4c654949894265d2f2e1cfa11
victorgboynton <victorgboynton@gmail.com>
2024-02-21 04:22:49 -0700

    Moved Project items to a reusable function

--- v2portfolio/app/ProjectList.tsx
+++ v2portfolio/app/ProjectList.tsx
@@ -1,8 +1,10 @@
 "use client";
 import React from "react";
-import { ProjectPreview } from "./ProjectPreview";
-import { ScrollAnimation } from "./ScrollAnimation";
-import { ProjectDescriptionArray } from "./ProjectDescriptionArray";
+import {
+	ProjectDescriptionArray,
+	ProjectDescriptionProps,
+} from "./ProjectDescriptionArray";
+import { AnimatedLinkToProjectPage } from "./AnimatedLinkToProjectPage";
 
 export function ProjectList() {
 	return (
@@ -14,50 +16,18 @@ export function ProjectList() {
 				<h1 className="text-center text-3xl font-bold mb-10">My Projects:</h1>
 				<div className="flex justify-center mx-auto">
 					<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-						<ScrollAnimation
-							inAnimation="animate-[fade-in_3s_forwards]"
-							outAnimation=""
-						>
-							<ProjectPreview
-								picture="/CookSuccess.webp"
-								altText="Picture of the CookSuccess Landing Page"
-								name="CookSuccess"
-								target={`/${ProjectDescriptionArray[0].slug}`}
-							/>
-						</ScrollAnimation>
-						<ScrollAnimation
-							inAnimation="animate-[fade-in_3s_forwards]"
-							outAnimation=""
-						>
-							<ProjectPreview
-								picture="/WhackAFood.webp"
-								altText="Picture of the Wack-a-Food load screen"
-								name="Whack-A-Food"
-								target="/"
-							/>
-						</ScrollAnimation>
-						<ScrollAnimation
-							inAnimation="animate-[fade-in_3s_forwards]"
-							outAnimation=""
-						>
-							<ProjectPreview
-								picture="/gameForceOne.webp"
-								altText="Picture of the Game Force Landing Page"
-								name="Game Force"
-								target="/"
-							/>
-						</ScrollAnimation>
-						<ScrollAnimation
-							inAnimation="animate-[fade-in_3s_forwards]"
-							outAnimation=""
-						>
-							<ProjectPreview
-								picture="/hopefulHumans.webp"
-								altText="Picture of the Hopeful Humans Landing Page"
-								name="Hopeful Humans"
-								target="/"
-							/>
-						</ScrollAnimation>
+						{ProjectDescriptionArray.map((project: ProjectDescriptionProps) => {
+							return (
+								<div key={project.id}>
+									<AnimatedLinkToProjectPage
+										picture={project.picture}
+										altText={project.alt}
+										name={project.projectName}
+										target={`/${project.slug}`}
+									/>
+								</div>
+							);
+						})}
 					</div>
 				</div>
 			</div>
--- v2portfolio/app/[slug]/page.tsx
+++ v2portfolio/app/[slug]/page.tsx
@@ -1,31 +1,32 @@
 "use client";
 import { stringify } from "querystring";
 import {
-  ProjectDescriptionArray,
-  ProjectDescriptionProps,
+	ProjectDescriptionArray,
+	ProjectDescriptionProps,
 } from "../ProjectDescriptionArray";
 import Image from "next/image";
 
 export default function Page({ params }: { params: { slug: string } }) {
-  const project = ProjectDescriptionArray.find((e: ProjectDescriptionProps) => {
-    e.slug === params.slug;
-    return e;
-  });
-  if (!project) {
-    return <div>Project Not Found</div>;
-  }
-  return (
-    <div>
-      <div>{project.projectName}</div>
-      <div>{project.slug}</div>
-      <div className="relative w-72 h-60">
-        <Image
-          src={project.picture}
-          alt="The Landing page of the website"
-          fill
-        />
-      </div>
-      <p>{project.description}</p>
-    </div>
-  );
+	const project = ProjectDescriptionArray.find((e: ProjectDescriptionProps) => {
+		if (e.slug === params.slug) {
+			return e;
+		}
+	});
+	if (!project) {
+		return <div>Project Not Found</div>;
+	}
+	return (
+		<div className="mt-10 text-white text-xl w-[90vw] mx-auto">
+			<h1 className="text-center text-3xl">{project.projectName}</h1>
+			<div className="relative w-72 h-60">
+				<Image
+					src={project.picture}
+					alt="The Landing page of the website"
+					fill
+					className="object-scale-down"
+				/>
+			</div>
+			<p>{project.description}</p>
+		</div>
+	);
 }
