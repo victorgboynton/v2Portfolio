bfe393224b84bf357ca5d5aee4167a5e2aae283a
Victor <victorgboynton@gmail.com>
2024-02-21 22:32:24 -0700

    Styling work on project pages

--- v2portfolio/app/Navbar.tsx
+++ v2portfolio/app/Navbar.tsx
@@ -6,78 +6,84 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export function Navbar() {
-	const [isNavOpen, setIsNavOpen] = useState(false);
-	const [showNav, setShowNav] = useState(true);
-	const [lastScrollY, setLastScrollY] = useState(0);
-	const handleSetIsNavOpen = () => setIsNavOpen((prev) => !prev);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [showNav, setShowNav] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const handleSetIsNavOpen = () => setIsNavOpen((prev) => !prev);
 
-	const controlSetShowNav = () => {
-		if (typeof window !== "undefined") {
-			if (window.scrollY > lastScrollY) {
-				setShowNav(false);
-			} else {
-				setShowNav(true);
-			}
-			setLastScrollY(window.scrollY);
-		}
-	};
-	useEffect(() => {
-		if (typeof window !== "undefined") {
-			window.addEventListener("scroll", controlSetShowNav);
-		}
-		return () => {
-			window.removeEventListener("scroll", controlSetShowNav);
-		};
-	});
+  const controlSetShowNav = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY > lastScrollY) {
+        setShowNav(false);
+      } else {
+        setShowNav(true);
+      }
+      setLastScrollY(window.scrollY);
+    }
+  };
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlSetShowNav);
+    }
+    return () => {
+      window.removeEventListener("scroll", controlSetShowNav);
+    };
+  });
 
-	return (
-		<header
-			className={`fixed top-0 w-[100vw] z-50 ${
-				showNav ? "translate-y-0" : "-translate-y-full"
-			} transition-transform duration-300 ease-in-out`}
-		>
-			<div className="relative flex flex-row justify-between sm:h-[5vh] sm:pl-5 sm:pr-10 bg-purple-950 items-center z-50;">
-				{isNavOpen ? <p></p> : <h1 className="text-white">Victor Boynton</h1>}
-				<button className="sm:hidden" onClick={handleSetIsNavOpen}>
-					{isNavOpen ? (
-						<div
-							className={`fixed inset-0 bg-gradient-to-b from-purple-800 to-purple-950  h-[100vh] w-[100vw] text-white flex flex-col pt-[25vh] justify-between`}
-						>
-							<div className="flex flex-col items-center text-2xl space-y-10">
-								<NavButton target={"/"}>Home</NavButton>
-								<NavButton target={"/#aboutMeSection"}>About</NavButton>
-								<NavButton target={"/#projects"}>Projects</NavButton>
-								<NavButton target={"/#contact"}>Contact Me</NavButton>
-							</div>
-							<div className="bg-gray-950 items-center py-[2vh]">
-								<button>Close</button>
-							</div>
-						</div>
-					) : (
-						<Image src={burger} height={30} width={30} alt="burger" />
-					)}
-				</button>
-				<div className="hidden sm:flex sm:h-[5vh] items-center">
-					<div className="flex flex-row items-center justify-between space-x-4 text-white">
-						<NavButton target={"/"}>Home</NavButton>
-						<NavButton target={"/#aboutMeSection"}>About</NavButton>
-						<NavButton target={"/#projects"}>Projects</NavButton>
-						<NavButton target={"/#contact"}>Contact Me</NavButton>
-					</div>
-				</div>
-			</div>
-		</header>
-	);
+  return (
+    <header
+      className={`fixed top-0 w-[100vw] font-robotoSlab z-50 ${
+        showNav ? "translate-y-0" : "-translate-y-full"
+      } transition-transform duration-300 ease-in-out`}
+    >
+      <div className="relative flex flex-row justify-between sm:h-[5vh] sm:pl-5 sm:pr-10 bg-purple-950 items-center z-50;">
+        {isNavOpen ? (
+          <p></p>
+        ) : (
+          <Link href="/">
+            <h1 className="text-white">Victor Boynton</h1>
+          </Link>
+        )}
+        <button className="sm:hidden" onClick={handleSetIsNavOpen}>
+          {isNavOpen ? (
+            <div
+              className={`fixed inset-0 bg-gradient-to-b from-purple-800 to-purple-950  h-[100vh] w-[100vw] text-white flex flex-col pt-[25vh] justify-between`}
+            >
+              <div className="flex flex-col items-center text-2xl space-y-10">
+                <NavButton target={"/"}>Home</NavButton>
+                <NavButton target={"/#aboutMeSection"}>About</NavButton>
+                <NavButton target={"/#projects"}>Projects</NavButton>
+                <NavButton target={"/#contact"}>Contact Me</NavButton>
+              </div>
+              <div className="bg-gray-950 items-center py-[2vh]">
+                <button>Close</button>
+              </div>
+            </div>
+          ) : (
+            <Image src={burger} height={30} width={30} alt="burger" />
+          )}
+        </button>
+        <div className="hidden sm:flex sm:h-[5vh] items-center">
+          <div className="flex flex-row items-center justify-between space-x-4 text-white">
+            <NavButton target={"/"}>Home</NavButton>
+            <NavButton target={"/#aboutMeSection"}>About</NavButton>
+            <NavButton target={"/#projects"}>Projects</NavButton>
+            <NavButton target={"/#contact"}>Contact Me</NavButton>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 type navButton = {
-	children: string;
-	target: string;
+  children: string;
+  target: string;
 };
 function NavButton(props: navButton) {
-	const pathname = usePathname();
-	return (
-		<Link href={props.target} className={`hover:bg-purple-700 p-1 rounded-md`}>
-			{props.children}
-		</Link>
-	);
+  const pathname = usePathname();
+  return (
+    <Link href={props.target} className={`hover:bg-purple-700 p-1 rounded-md`}>
+      {props.children}
+    </Link>
+  );
 }
--- v2portfolio/app/[slug]/page.tsx
+++ v2portfolio/app/[slug]/page.tsx
@@ -1,46 +1,53 @@
 "use client";
 import { stringify } from "querystring";
 import {
-	ProjectDescriptionArray,
-	ProjectDescriptionProps,
+  ProjectDescriptionArray,
+  ProjectDescriptionProps,
 } from "../ProjectDescriptionArray";
 import Image from "next/image";
 
 export default function Page({ params }: { params: { slug: string } }) {
-	const project = ProjectDescriptionArray.find((e: ProjectDescriptionProps) => {
-		if (e.slug === params.slug) {
-			return e;
-		}
-	});
-	if (!project) {
-		return <div>Project Not Found</div>;
-	}
-	const progressColor =
-		project.status.toLowerCase() === "completed"
-			? "text-green-500"
-			: "text-yellow-400";
-	return (
-		<div className="mt-20 text-white text-xl w-[90vw] mx-auto">
-			<div className="relative w-72 h-60 md:w-96 md:h-72 lg:w-[450px] lg:h-[300px] mx-auto">
-				<Image
-					src={project.picture}
-					alt="The Landing page of the website"
-					fill
-					className="object-scale-down"
-				/>
-			</div>
-			<h1 className="text-center text-[2.5em] mb-10">{project.projectName}</h1>
-			<p>{project.description}</p>
-			<div className="flex flex-row">
-				<h2>Skills Used:&nbsp;</h2>
-				{project.skills.map((e: string, index: number) => {
-					return <div key={index}>{e}&nbsp;</div>;
-				})}
-			</div>
-			<div className="flex flex-row text-2xl">
-				<p>Status:&nbsp;</p>
-				<p className={`${progressColor}`}>{project.status}</p>
-			</div>
-		</div>
-	);
+  const project = ProjectDescriptionArray.find((e: ProjectDescriptionProps) => {
+    if (e.slug === params.slug) {
+      return e;
+    }
+  });
+  if (!project) {
+    return <div>Project Not Found</div>;
+  }
+  const progressColor =
+    project.status.toLowerCase() === "completed"
+      ? "text-green-500"
+      : "text-yellow-400";
+  return (
+    <div className="mt-20 text-white text-xl w-[90vw] mx-auto space-y-3 font-robotoSlab">
+      <div className="relative w-72 h-60 md:w-96 md:h-72 lg:w-[600px] lg:h-[300px] mx-auto">
+        <Image
+          src={project.picture}
+          alt="The Landing page of the website"
+          fill
+          className="object-scale-down"
+        />
+      </div>
+      <h1 className="text-center text-[2em] sm:text-[2.5em] font-bold pb-8">
+        {project.projectName}
+      </h1>
+      <div className="flex flex-row flex-wrap items-center">
+        <h2>Skills Used:&nbsp;</h2>
+        {project.skills.map((e: string, index: number) => {
+          return (
+            <div key={index} className=" bg-black mx-1 p-1 rounded-xl">
+              {e}
+            </div>
+          );
+        })}
+      </div>
+      <p>{project.description}</p>
+
+      <div className="flex flex-row text-2xl">
+        <p>Status:&nbsp;</p>
+        <p className={`${progressColor}`}>{project.status}</p>
+      </div>
+    </div>
+  );
 }
