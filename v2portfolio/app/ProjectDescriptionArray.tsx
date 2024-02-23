dd280fb3784a9b658e02b34504c8eefe3845c0a0
Victor <victorgboynton@gmail.com>
2024-02-23 03:32:19 -0700

    Spelling fixes

--- v2portfolio/app/ProjectDescriptionArray.tsx
+++ v2portfolio/app/ProjectDescriptionArray.tsx
@@ -14,14 +14,14 @@ export const ProjectDescriptionArray: ProjectDescriptionProps[] = [
     description: (
       <div>
         <p>
-          &apos;CookSuccess&apos; is where I honed my backend skills. The
-          inspiration for &apos;CookSuccess&apos; came from having a lot of
-          ingredients in my kitchen, and no idea what to do with them.
+          'CookSuccess' is where I honed my backend skills. The inspiration for
+          'CookSuccess' came from having a lot of ingredients in my kitchen, and
+          no idea what to do with them.
         </p>
         <br />
         <p>
-          The challange from this website came from my relative new-ness to the
-          techstack used. If I were to remake it today, it would take me far
+          The challenge from this website came from my relative newness to the
+          tech stack used. If I were to remake it today, it would take me far
           less time.
         </p>
       </div>
@@ -39,13 +39,13 @@ export const ProjectDescriptionArray: ProjectDescriptionProps[] = [
     description: (
       <div>
         <p>
-          &apos;Whack-a-Food&apos; is unique to this portfolio because it is not
-          a website, and it was from a tutorial. It is the capstone project for
-          the Junior Programmer Course for game development from Unity.{" "}
+          'Whack-a-Food' is unique to this portfolio because it is not a
+          website, and it was from a tutorial. It is the capstone project for
+          the Junior Programmer Course for game development from Unity.
         </p>
         <br />
         <p>
-          All of the assests were provided by Unity. While I love game
+          All of the assets were provided by Unity. While I love game
           development, I do not have a dedicated artist, and thus the tutorial
           is as far as I will go with game development for now.
         </p>
@@ -65,26 +65,26 @@ export const ProjectDescriptionArray: ProjectDescriptionProps[] = [
       <div>
         <p>
           This project taught me a whole different lesson than the other
-          projects on this site. Two whole different lessons, to be exact.{" "}
+          projects on this site. Two whole different lessons, to be exact.
         </p>
         <br />
         <p>
           <strong className="text-2xl">Lesson One: Working with a team</strong>
           <br /> Working with a team is a whole different beast compared to
-          working solo. I was the lead on this, and the demands of responsiblity
-          were high with a very new programmer working with me. Combining that
-          with learning the intricasies of Git, this project really opened my
-          eyes to the software development process.
+          working solo. I was the lead on this, and the demands of
+          responsibility were high with a very new programmer working with me.
+          Combining that with learning the intricacies of Git, this project
+          really opened my eyes to the software development process.
         </p>
         <br />
         <p>
           <strong className="text-2xl">
-            Lesson Two: Communicate with project owner
+            Lesson Two: Communicate with the project owner
           </strong>
           <br /> This lesson is why the website is not hosted anywhere. After
-          working with my partner and completing the site, we approched the
+          working with my partner and completing the site, we approached the
           business owner about replacing his old, no CSS HTML website with this.
-          He politly declined, and sent us on our way.
+          He politely declined and sent us on our way.
         </p>
       </div>
     ),
@@ -99,8 +99,7 @@ export const ProjectDescriptionArray: ProjectDescriptionProps[] = [
     skills: ["React", "TailwindCSS", "Nextjs", "Git"],
     description: (
       <div>
-        <p className="text-white ">
-          {" "}
+        <p className="text-white">
           This project involved creating a website for a charity named{" "}
           <strong>&apos;Hopeful Humans&apos;</strong>. The goal was to design a
           platform that not only serves the charity&apos;s needs but also stands
@@ -109,15 +108,16 @@ export const ProjectDescriptionArray: ProjectDescriptionProps[] = [
         <br />
         <p>
           My aspiration for this project is for it to be recognized as the
-          magnum opus of my portfolio, showing my technical acumin and strength
-          as a team-lead.
+          magnum opus of my portfolio, showcasing my technical acumen and
+          strength as a team lead.
         </p>
         <br />
         <p>
-          This is by far my most complicated project. Everything you see on this
-          website was had crafted by me. I used no libraries external to
-          Next.js. The one crutch I used was for the contact page, where I used
-          an outside mail server and API to send the contact information.
+          This was by far my most complicated project. Everything you see on
+          this website was handcrafted by me. I used no libraries external to
+          Next.js. The one crutch I used was for the contact page, where I
+          utilized an external mail server and API to send the contact
+          information.
         </p>
       </div>
     ),
